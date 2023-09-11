414f3ef3a2a74163bad172b50523fa15ec50f6a3
Zicka67 <art.bookk@hotmail.fr>
2023-09-11 11:13:26 +0200

    Mise en page de success payment et de l'animation checkmark

---
+++ public/js/checkmark.js
@@ -0,0 +1,13 @@
+//Checkmark
+var animation = lottie.loadAnimation({
+    container: document.getElementById('lottie-container'),
+    renderer: 'svg', 
+    loop: false, 
+    autoplay: true, 
+    path: 'js/successfully-done.json' 
+  });
+
+  
+  setTimeout(function() {
+      window.location.href = "http://localhost:8000/showCours";
+  }, 3800); 
