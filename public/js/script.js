faac4957d8ade0f682e989c71671e4914c904acb
Zicka67 <art.bookk@hotmail.fr>
2023-09-12 16:54:53 +0200

    Mise en place d'un système de visibility de mot de passe, BUGGED

--- public/js/script.js
+++ public/js/script.js
@@ -57,6 +57,18 @@ document.addEventListener("DOMContentLoaded", function() {
 
 // ***************************
 
+// let eyeicon = document.getElementById("eyeicon");
+// let password = document.getElementById("password");
+
+
+// eyeicon.onclick = function(){
+//     if(password.type == "password"){
+//         password.type = "text";
+//     }else {
+//         password.type = "password";
+//     }
+// }
+
 
 
 
