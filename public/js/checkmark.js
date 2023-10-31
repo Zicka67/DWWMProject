66671cd5756eb7f5eff04928dda749f0c3d78031
Zicka67 <art.book@hotmail.fr>
2023-11-01 00:52:37 +0100

    Mise en place de lottie pour l'animation checked

--- public/js/checkmark.js
+++ public/js/checkmark.js
@@ -1,13 +1,4 @@
-//Checkmark
-var animation = lottie.loadAnimation({
-    container: document.getElementById('lottie-container'),
-    renderer: 'svg', 
-    loop: false, 
-    autoplay: true, 
-    path: 'js/successfully-done.json' 
-  });
 
-  
   setTimeout(function() {
       window.location.href = "http://localhost:8000/showCours";
-  }, 380000); 
+  }, 2800);     
